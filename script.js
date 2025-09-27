1cf9333c4b2acac4aba11091c0fe806716c2f1ca
Paulo <124407850+PauloSantos2002@users.noreply.github.com>
2025-09-26 21:32:05 -0300

    update js
    adição da função de concluido e melhorias do css

--- script.js
+++ script.js
@@ -1,18 +1,29 @@
 const form = document.getElementById('formulario')
 const list = document.getElementById('lista')
 
+
 form.addEventListener('submit', function(event){
     event.preventDefault()
+
+    const input = {
+        tarefa: document.getElementById('tarefaInput').value,
+        categoria: document.getElementById('categoriaInput').value,
+        prioridade: document.querySelector('input[name=prioridade]:checked').value,
+        dataInicio: document.getElementById('dataInicio').value,
+        dataFim: document.getElementById('dataFim').value,
+        notas: document.getElementById('notasInput').value
     
+    }
+    //Adicionar Tarefa
     const item = document.createElement('li')
     item.classList.add('itemlista')
     item.innerHTML = `
             <div class='itemlista_conteudo'>
-                <h2> ${form.tarefa.value} </h2>
-                <p> ${form.categoria.value} </p>
-                <p> ${form.prioridade.value} </p>
-                <p> ${form.data.value} </p>
-                <p> ${form.notas.value} </p>
+                <h2> ${input.tarefa} </h2>
+                <p> ${input.categoria} </p>
+                <p> ${input.prioridade} </p>
+                <p> ${input.dataInicio} - ${input.dataFim}</p>
+                <p> ${input.notas} </p>
             </div>
             <div class='itemlista_botao'>
                 <p class='concluir'> ✅</p>
@@ -21,11 +32,17 @@ form.addEventListener('submit', function(event){
             </div>
         
     `
-
-     item.querySelector('.excluir').addEventListener('click', () => {
-        item.remove();
-    });
     list.appendChild(item)
     form.reset()
+    //Remover Tarefa
+    item.querySelector('.excluir').addEventListener('click', () => {
+        item.remove();
+    });
+    item.querySelector('.concluir').addEventListener('click', () => {
+        item.style.backgroundColor = "green";
+    });
+
+    
+
     
 })
